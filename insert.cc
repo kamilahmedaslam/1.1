fa138bc73d56140706556e84c4821f2a281236d3
lilsteviebird <stevenli880@gmail.com>
2022-03-04 21:08:12 -0500

    Update insert.cc
    Fixed abrt error in insert

--- insert.cc
+++ insert.cc
@@ -1,3 +1,9 @@
+//
+//  insert.cpp
+//  
+//
+//  Created by Steven Li on 3/4/22.
+//
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -11,33 +17,36 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     FILE *given_heap = fopen(argv[1], "rb+");
-    FILE *csv_page = fopen(argv[2], "r");
+    ifstream csv_file(argv[2]);
     int page_size = atoi(argv[3]);
+//    FILE *csv_page = fopen("128_tuples.csv", "r");
+    
     Heapfile *heap = (Heapfile *)malloc(sizeof(Heapfile));
     init_heapfile(heap, page_size, given_heap);
     
     string line;
-    char tuples[num_attributes * attribute_size];
+//    char tuples[num_attributes * attribute_size];
     
     int r_size = num_attributes;
     Page *temp_page = (Page *)malloc(sizeof(Page));
     init_fixed_len_page(temp_page, page_size, r_size);
-    
-    while(fgets(tuples, page_size, csv_page)){
-            Record temp_record;
-            line = tuples;
-            line.erase(remove(line.begin(), line.end(), ','), line.end());
-            fixed_len_read((char*)line.c_str(), r_size, &temp_record);
-            int slot_index = add_fixed_len_page(temp_page, &temp_record);
+    while(getline(csv_file, line)){
+        Record temp_record;
+
+        line.erase(remove(line.begin(), line.end(), ','), line.end());
+//        printf("%s", line.c_str());
+        fixed_len_read((char*)line.c_str(), r_size, &temp_record);
+        int slot_index = add_fixed_len_page(temp_page, &temp_record);
         
-            if (slot_index==-1){
-                PageID add_me = alloc_page(heap);
-                write_page(temp_page,heap,add_me);
-                init_fixed_len_page(temp_page, page_size, r_size);
-                add_fixed_len_page(temp_page, &temp_record);
-            }
+        if (slot_index==-1){
+            PageID add_me = alloc_page(heap);
+            write_page(temp_page,heap,add_me);
+            init_fixed_len_page(temp_page, page_size, r_size);
+            add_fixed_len_page(temp_page, &temp_record);
         }
-    
+    }
+        
+    printf("\nfinished\n");
     return 0;
 
     
